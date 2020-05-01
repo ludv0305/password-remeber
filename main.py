1e3dccc1e952d54a9eee36304cc05362efd65140
ludv0305 <61505141+ludv0305@users.noreply.github.com>
2020-05-01 09:25:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ choice1 = input("do you want to sign in or sign up? ").lower()
 if choice1 == "sign in":
     username = open("login.json", "r")
     users = username.read()
-    ask = input("hvad er dit brugernavn? ")
+    ask = input("what is your username? ")
     if ask == users:
-        print("velkommen")
+        print("velcome")
         choice2 = input("do you want to find a old password or enter a new one? ")
         if choice2 == "enter" or choice2 == "enter a new one" or choice2 == "new one" or choice2 == "a new one":
             new_file = input("what do you want to be the name of your file? (remeber to put .json at the end) ")
@@ -15,8 +15,8 @@ if choice1 == "sign in":
             dump2 = json.dumps(new_password)
             with open(new_file, "w") as nf:
                 nf.write(new_password)
-        elif choice2 == "find" or choice2 == "find an old file" or choice2 == "find an old password":
-            which = input("hvad hedder filen som passwordet er gemt i")
+        elif choice2 == "find" or choice2 == "find an old file" or choice2 == "find an old password" or choice2 == "old password":
+            which = input("what is the name of the file the password is stored in? ")
             find = open(which, "r")
             passwords = find.read()
             print(passwords)
